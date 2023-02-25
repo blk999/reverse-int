c29bf5cac665ffa800b4b5c7911f0a6a34793049
blk999 <bbbulka@gmail.com>
2023-02-25 20:02:06 +0400

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    
+    let str = Math.abs(n).toString();
+    let reverse = "";
+      for (i = 0; i < str.length; i++) {
+          reverse = str[i] + reverse;
+      };
+     
+      return +reverse;
 }
